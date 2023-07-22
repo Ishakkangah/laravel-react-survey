3b3a63522d2afaf46e3233cf12b5f169d1cf6ea0
Ishakkangah <ishakkangah@gmail.com>
2023-07-22 17:51:07 +0700

    Create Guest Layout, DefaultLayout, PageComponent

---
+++ react/src/views/Surveys.jsx
@@ -0,0 +1,29 @@
+export default function Surveys() {
+  return (
+    <>
+      <header className="bg-white shadow">
+        <div className="mx-auto max-w-7xl px-4 py-6 sm:px-6 lg:px-8">
+          <h1 className="text-3xl font-bold tracking-tight text-gray-900">
+            Surveys
+          </h1>
+        </div>
+      </header>
+      <main>
+        <div className="mx-auto max-w-7xl py-6 sm:px-6 lg:px-8">
+          Lorem ipsum dolor sit amet consectetur, adipisicing elit. Soluta
+          obcaecati provident, facilis velit explicabo nostrum suscipit, porro
+          laudantium nemo dignissimos tenetur eum. Deleniti nam commodi tempora
+          ea sapiente reprehenderit perferendis consectetur perspiciatis,
+          dolorum quasi cupiditate nulla, cumque asperiores corporis inventore
+          eius consequuntur! Dolores rem exercitationem delectus nisi veniam
+          necessitatibus amet dolor pariatur autem! Corporis vero eius dolores
+          minus porro exercitationem. Incidunt aliquid vero reprehenderit, sequi
+          pariatur omnis ipsam cum, molestiae optio dolor sint in iste non,
+          aliquam quam quaerat saepe odio est nulla? Vitae a quae dignissimos
+          pariatur magnam, sunt, consequuntur nostrum nobis earum quisquam,
+          ducimus illum repellendus voluptate. Iure?
+        </div>
+      </main>
+    </>
+  );
+}
